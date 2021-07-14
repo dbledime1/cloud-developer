10800f22194b6d165715dfa4afbe2d6a246bc9dd
Adrian Mitchell <dbledime@yahoo.com>
2021-07-14 12:51:27 -0500

    added TypeScript (Response, Request) to server.tx

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -33,7 +33,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/filteredimage", async ( req, res ) => {
+  app.get( "/filteredimage", async ( req:Request, res:Response ) => {
     const { image_url } = req.query;
     if (image_url) {
       filterImageFromURL(image_url).then((response) => {
